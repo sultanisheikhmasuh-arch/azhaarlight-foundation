be8df4e3915b435edc85794f6d602e11a360d02e
sultanisheikhmasuh-arch <sultanisheikhmasuh@gmail.com>
2026-05-02 20:24:45 +0200

    Create page.tsx

---
+++ app/about/page.tsx
@@ -0,0 +1,13 @@
+import { Header } from "@/components/header"
+import { About } from "@/components/about"
+import { Footer } from "@/components/footer"
+
+export default function AboutPage() {
+  return (
+    <main className="min-h-screen">
+      <Header />
+      <About />
+      <Footer />
+    </main>
+  )
+}
