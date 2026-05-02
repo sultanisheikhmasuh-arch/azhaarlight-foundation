e536b1b9012cd8455abc70fa2dffb3b144ebafe0
sultanisheikhmasuh-arch <sultanisheikhmasuh@gmail.com>
2026-05-02 23:26:10 +0200

    Create page.tsx

---
+++ app/vision/page.tsx
@@ -0,0 +1,13 @@
+import { Header } from "@/components/header"
+import { Vision } from "@/components/vision"
+import { Footer } from "@/components/footer"
+
+export default function VisionPage() {
+  return (
+    <main className="min-h-screen">
+      <Header />
+      <Vision />
+      <Footer />
+    </main>
+  )
+}
