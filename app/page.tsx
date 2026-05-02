ade49fdbd60c48dd9878c01564b8c04e3384c5f8
sultanisheikhmasuh-arch <sultanisheikhmasuh@gmail.com>
2026-05-02 18:50:23 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,5 @@
 import { Header } from "@/components/header"
 import { Hero } from "@/components/hero"
-import { About } from "@/components/about"
-import { Mission } from "@/components/mission"
-import { Vision } from "@/components/vision"
-import { Values } from "@/components/values"
-import { HowToHelp } from "@/components/how-to-help"
-import { Contact } from "@/components/contact"
 import { Footer } from "@/components/footer"
 
 export default function Home() {
@@ -13,12 +7,6 @@ export default function Home() {
     <main className="min-h-screen">
       <Header />
       <Hero />
-      <About />
-      <Mission />
-      <Vision />
-      <Values />
-      <HowToHelp />
-      <Contact />
       <Footer />
     </main>
   )
