fb80e42860d6c0e6a11317addbd4de8cbd12d192
sultanisheikhmasuh-arch <sultanisheikhmasuh@gmail.com>
2026-05-03 13:29:55 +0200

    Create page.tsx

---
+++ app/valeurs/page.tsx
@@ -0,0 +1,13 @@
+import { Header } from "@/components/header"
+import { Values } from "@/components/values"
+import { Footer } from "@/components/footer"
+
+export default function ValeursPage() {
+  return (
+    <main className="min-h-screen">
+      <Header />
+      <Values />
+      <Footer />
+    </main>
+  )
+}
