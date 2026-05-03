2b6f4b86b4a8eb2f0c517cdb7c708e44514b0e81
sultanisheikhmasuh-arch <sultanisheikhmasuh@gmail.com>
2026-05-03 20:39:41 +0200

    Create reports.tsx

---
+++ components/components/reports.tsx
@@ -0,0 +1,13 @@
+import { Header } from "@/components/header"
+import { Reports } from "@/components/reports"
+import { Footer } from "@/components/footer"
+
+export default function RapportsPage() {
+  return (
+    <main className="min-h-screen">
+      <Header />
+      <Reports />
+      <Footer />
+    </main>
+  )
+}
