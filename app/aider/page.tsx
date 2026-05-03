c31080bd7a5603826c8764d9bfe6ab4f82dab06e
sultanisheikhmasuh-arch <sultanisheikhmasuh@gmail.com>
2026-05-03 14:04:19 +0200

    Create page.tsx

---
+++ app/aider/page.tsx
@@ -0,0 +1,13 @@
+import { Header } from "@/components/header"
+import { HowToHelp } from "@/components/how-to-help"
+import { Footer } from "@/components/footer"
+
+export default function AiderPage() {
+  return (
+    <main className="min-h-screen">
+      <Header />
+      <HowToHelp />
+      <Footer />
+    </main>
+  )
+}
