135389cd94efa40eb4ea793616debc5ebc4eb7c5
sultanisheikhmasuh-arch <sultanisheikhmasuh@gmail.com>
2026-05-03 22:26:43 +0200

    Create page.tsx

---
+++ app/recherche/page.tsx
@@ -0,0 +1,13 @@
+import { Header } from "@/components/header"
+import { SearchPage } from "@/components/search"
+import { Footer } from "@/components/footer"
+
+export default function RecherchePage() {
+  return (
+    <main className="min-h-screen">
+      <Header />
+      <SearchPage />
+      <Footer />
+    </main>
+  )
+}
