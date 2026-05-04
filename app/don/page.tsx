722a52bb5a1050bd4fe84614a4d2ddf8588c4bee
sultanisheikhmasuh-arch <sultanisheikhmasuh@gmail.com>
2026-05-04 03:01:45 +0200

    Create page.tsx

---
+++ app/don/page.tsx
@@ -0,0 +1,13 @@
+import { Header } from "@/components/header"
+import { Don } from "@/components/don"
+import { Footer } from "@/components/footer"
+
+export default function DonPage() {
+  return (
+    <main className="min-h-screen">
+      <Header />
+      <Don />
+      <Footer />
+    </main>
+  )
+}
