563b542dd00cde2933007d4a5f4175fe6b48e13b
sultanisheikhmasuh-arch <sultanisheikhmasuh@gmail.com>
2026-05-04 03:05:56 +0200

    Create page.tsx

---
+++ app/partenaires/page.tsx
@@ -0,0 +1,13 @@
+import { Header } from "@/components/header"
+import { Partenaires } from "@/components/partenaires"
+import { Footer } from "@/components/footer"
+
+export default function PartenairesPage() {
+  return (
+    <main className="min-h-screen">
+      <Header />
+      <Partenaires />
+      <Footer />
+    </main>
+  )
+}
