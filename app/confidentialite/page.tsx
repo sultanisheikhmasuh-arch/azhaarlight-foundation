c126dd2eb9cadb4738efcbface340d1daec04d8b
sultanisheikhmasuh-arch <sultanisheikhmasuh@gmail.com>
2026-05-04 03:03:41 +0200

    Create page.tsx

---
+++ app/confidentialite/page.tsx
@@ -0,0 +1,13 @@
+import { Header } from "@/components/header"
+import { Privacy } from "@/components/privacy"
+import { Footer } from "@/components/footer"
+
+export default function PrivacyPage() {
+  return (
+    <main className="min-h-screen">
+      <Header />
+      <Privacy />
+      <Footer />
+    </main>
+  )
+}
