2b809c5eb91c9f5135e0c561b6563c006d3863cf
sultanisheikhmasuh-arch <sultanisheikhmasuh@gmail.com>
2026-05-04 03:04:44 +0200

    Create page.tsx

---
+++ app/cgu/page.tsx
@@ -0,0 +1,13 @@
+import { Header } from "@/components/header"
+import { CGU } from "@/components/cgu"
+import { Footer } from "@/components/footer"
+
+export default function CGUPage() {
+  return (
+    <main className="min-h-screen">
+      <Header />
+      <CGU />
+      <Footer />
+    </main>
+  )
+}
